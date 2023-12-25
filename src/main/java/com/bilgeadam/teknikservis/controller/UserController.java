cb8324dbdaab3b3631168370242ba321619f7e1a
mustafa <ilikmustafa03@gmail.com>
2023-12-25 12:22:25 +0300

    SignUp islemleri

--- src/main/java/com/bilgeadam/teknikservis/controller/UserController.java
+++ src/main/java/com/bilgeadam/teknikservis/controller/UserController.java
@@ -1,4 +1,45 @@
 package com.bilgeadam.teknikservis.controller;
 
+import com.bilgeadam.teknikservis.model.User;
+import com.bilgeadam.teknikservis.repository.UserRepository;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping(path = "save" , consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> save (@RequestBody User user){
+        try {
+            boolean result = userRepository.save(user);
+
+            if(result){
+                return ResponseEntity.ok("User Başarıyla Kaydedildi");
+            }
+            else {
+                return ResponseEntity.internalServerError().body("User kaydedilemedi");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("User Kaydedilemedi");
+        }
+
+    }
+    @GetMapping(path ="getall" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<User>> getall(){
+        return ResponseEntity.ok(userRepository.getall());
+    }
+
 }
+
--- src/main/java/com/bilgeadam/teknikservis/repository/UserRepository.java
+++ src/main/java/com/bilgeadam/teknikservis/repository/UserRepository.java
@@ -1,4 +1,48 @@
 package com.bilgeadam.teknikservis.repository;
 
+import com.bilgeadam.teknikservis.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class UserRepository {
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
+
+    private PasswordEncoder passwordEncoder;
+
+
+    public UserRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate, JdbcTemplate jdbcTemplate, PasswordEncoder passwordEncoder) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+        this.jdbcTemplate = jdbcTemplate;
+        this.passwordEncoder= passwordEncoder;
+    }
+
+
+    public boolean save (User user){
+
+        String sql = "insert into \"public\".\"USERS\" (\"username\",\"email\",\"password\",\"role\") values (:username, :email, :password, :role)";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("username" , user.getUsername());
+        paramMap.put("email" , user.getEmail());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        paramMap.put("password" , user.getPassword());
+        user.setRole("ROLE_USER");
+        paramMap.put("role" , user.getRole());
+        return  namedParameterJdbcTemplate.update(sql , paramMap) == 1;
+
+
+    }
+
+    public List<User> getall(){
+        return jdbcTemplate.query("select * from \"public\".\"USERS\" order by \"id\" asc", BeanPropertyRowMapper.newInstance(User.class));
+    }
 }
